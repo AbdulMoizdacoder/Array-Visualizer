d48d2012eb65e12e73972cb2d8e80420603e3d64
AbdulMoizdacoder <112655215+AbdulMoizdacoder@users.noreply.github.com>
2022-09-28 13:28:11 -0600

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let containerEl = document.getElementById("container");
 // Global Variable
 let myData = [];
 for(let n = 1; n <= 50; n++){
-    myData.push(Math.random()* 600);
+    
 }
 console.log(myData);
 
@@ -29,9 +29,22 @@ document.addEventListener("keydown", keydownHandler)
 
 function keydownHandler(event){
     // Modify the array........
-    for (let i = 0; i< myData.length; i++){
-        myData[i] = 350; 
+    if(event.keyCode === 38){
+        // Up arrow - add 600 to index 0
+        myData.splice(0, 0, Math.random()* 600);
+    } else if (event.keyCode === 40){
+        // Down Arrow - Remove element from middle
+        myData.splice(0, 1);
     }
+
+    //   if(event.keyCode === 38){
+    //     // Up arrow - push value to end of array
+    //     myData.push(Math.random()* 600);
+    // } else if (event.keyCode === 40){
+    //     // Down Arrow - Remove value from the end of array
+    //    let poppedValue = myData.pop()
+    //    console.log(poppedValue);
+    // }
 
     // Redraw Array
     drawArray();
